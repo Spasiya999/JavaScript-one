771d512648a6d4eccda40b579d9fb3ab767fdcb9
spasindu <spsd49@gmail.com>
2023-04-25 13:59:28 +0530

    operators

---
+++ assest/js/op.js
@@ -0,0 +1,46 @@
+// let cal = 4+1;
+ cal = 4*1;
+ cal = 4**1;
+ cal = 4%1;
+
+//  let cal = 4;
+ cal++;
+ cal--;
+
+ let num1 = 5
+ let num2 = 10
+ let num3 = 11
+
+ let add = num1 + num2 + num3
+    console.log('Add',add);
+
+ let sub = num2 - num1
+    console.log('Sub',sub)
+
+ let mul = num1 * num2
+    console.log('Mul',mul)
+
+ let div = num2 / num1
+    console.log('Div',div)
+
+ let mod = num3 % num1
+    console.log('Mod',mod)
+
+ let exp = num3 ** num1
+    console.log('Exp',exp);
+
+let inc = num1++
+console.log('Inc',inc);
+console.log('Inc',num1);
+
+let incD = ++num1
+console.log('Inc',incD);
+console.log('Inc',num1);
+
+let dec = num1++
+console.log('Dec',dec);
+console.log('Inc',num1);
+
+let decD = ++num1
+console.log('dec',decD);
+console.log('dec',num1);
