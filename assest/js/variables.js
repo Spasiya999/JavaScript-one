d43ad8ff4433eba693329fe7017ee84c54dbaf48
spasindu <spsd49@gmail.com>
2023-04-25 12:42:56 +0530

    Make Variables

---
+++ assest/js/variables.js
@@ -0,0 +1,17 @@
+// var " - is old "
+// let
+// const
+const paraOne = document.getElementById('div1')
+console.log(paraOne)
+let message = "Hello world";
+// message = 'HELLO';
+
+//same
+
+// paraOne.innerHTML = 'Hello';
+paraOne.innerHTML = message;
+
+var paraTwo;
+paraTwo = document.getElementById('#div2');
+console.log(paraTwo)
+var paraTwo = 'hhh';
